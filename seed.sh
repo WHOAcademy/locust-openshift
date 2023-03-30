b108001906895ee4193688cfebdd32bd7c953140
Krishna Madhavan <krishnamadhavan07@gmail.com>
2023-03-30 18:48:36 +0530

    fix: locust configuration on locust namespace

--- seed.sh
+++ seed.sh
@@ -22,7 +22,7 @@ kind: ConfigMap
 apiVersion: v1
 metadata:
   name: host-url
-  namespace: test-locust
+  namespace: locust
 data:
   ATTACKED_HOST: $hostName
 EOF1
@@ -39,7 +39,7 @@ kind: ConfigMap
 apiVersion: v1
 metadata:
   name: script-file
-  namespace: test-locust
+  namespace: locust
 data:
   locustfile.py: |
 $(cat $testFile | sed 's/^/    /')
@@ -52,6 +52,6 @@ cat config-map.yaml | oc apply -f -
 rm ./config-map.yaml
 
 # Update the environment variable to trigger a change
-oc project test-locust
+oc project locust
 oc set env dc/locust-master --overwrite CONFIG_HASH=`date +%s%N`
 oc set env dc/locust-slave --overwrite CONFIG_HASH=`date +%s%N`
